5a2f05d7426444529bf8cb7361ab89e5fd7ec221
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-05-19 12:02:47 -0700

    dasboard all products section added

--- src/app/dashboard/all-products/page.tsx
+++ src/app/dashboard/all-products/page.tsx
@@ -1,9 +1,47 @@
+import { TLaptop } from "@/types";
+import Image from "next/image";
 import React from "react";
 
-const AllProductsPage = () => {
+const AllProductsPage = async () => {
+  const res = await fetch("http://localhost:5000/laptop");
+  const laptopdata = await res.json();
+
   return (
     <div>
-      <h1>All Product</h1>
+      <h1 className="text-3xl text-green-600">All Products</h1>
+      <div>
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full bg-sky-300">
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Brand</th>
+                <th>Configuration</th>
+                <th>Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {laptopdata.map((laptop: TLaptop) => (
+                <tr key={laptop._id}>
+                  <td>
+                    <Image
+                      src={laptop.image}
+                      width={150}
+                      height={80}
+                      alt="image"
+                    />
+                  </td>
+                  <td>{laptop.name}</td>
+                  <td className="text-orange-600">{laptop.brand}</td>
+                  <td>{laptop.configuration}</td>
+                  <td className="text-red-500">${laptop.price}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
