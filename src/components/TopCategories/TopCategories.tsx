9f3ec298532281fc2ee633508ba8b61d03841cdf
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-05-20 01:35:32 -0700

    top categories brand added

--- src/components/TopCategories/TopCategories.tsx
+++ src/components/TopCategories/TopCategories.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { TLaptop } from "@/types";
-import TopCategoriesCard from "../ui/TopCategoriesCard";
+
+import { useRouter } from "next/navigation";
+
+import AsusLaptopCard from "../ui/AsusLaptopCard";
+import LenovoLaptopCard from "../ui/LenovoLaptopCard";
+import DellLaptopCard from "../ui/DellLaptopCard";
 
 const TopCategories = ({ data }: { data: TLaptop[] }) => {
-  const topCategory = data.filter(
-    (category: TLaptop) => category.top_category === true
-  );
+  const dellLaptops = data.filter((item) => item.brand == "Dell");
+  const asusLaptops = data.filter((item) => item.brand == "Asus");
+  const lenovoLaptops = data.filter((item) => item.brand == "Lenovo");
+
+  const router = useRouter();
   return (
     <div>
       <h1 className="text-4xl text-accent text-center my-4 border-y-2 border-emerald-400 w-[25%] mx-auto py-2">
@@ -14,10 +23,31 @@ const TopCategories = ({ data }: { data: TLaptop[] }) => {
         Laptops have revolutionized the way we interact with technology,
         transforming from clunky briefcase replacements into sleek and powerful
       </p>
-      <div className="md:grid grid-cols-3 gap-4 my-8">
-        {topCategory.slice(0, 3).map((category) => (
-          <TopCategoriesCard category={category} key={category._id} />
-        ))}
+      <div className="my-4">
+        <div>
+          <h1 className="text-2xl text-center text-blue-500">Brand: DELL</h1>
+          <div className="md:flex gap-4 my-2">
+            {dellLaptops.map((item) => (
+              <DellLaptopCard key={item._id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl text-center text-blue-500">Brand: Asus</h1>
+          <div className="md:flex gap-4 my-2">
+            {asusLaptops.map((item) => (
+              <AsusLaptopCard key={item._id} item={item} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl text-center text-blue-500">Brand: Lenovo</h1>
+          <div className="md:flex gap-4 my-2">
+            {lenovoLaptops.map((item) => (
+              <LenovoLaptopCard key={item._id} item={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ui/AsusLaptopCard.tsx
@@ -0,0 +1,18 @@
+import { TLaptop } from "@/types";
+import Image from "next/image";
+
+const AsusLaptopCard = ({ item }: { item: TLaptop }) => {
+  const { name, image, brand, configuration, price, product_rating, _id } =
+    item;
+  return (
+    <div>
+      <Image src={image} width={400} height={80} alt="image" />
+      <h1 className="text-2xl font-bold text-red-500">{brand}</h1>
+      <h1 className="text-green-500">{name}</h1>
+      <p>{configuration}</p>
+      <p className="text-orange-500">${price}</p>
+    </div>
+  );
+};
+
+export default AsusLaptopCard;
